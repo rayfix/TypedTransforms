46e6d9e4b574c555d9c83e0ccaeeeed71d3a8a0c
Ray Fix <rayfix@gmail.com>
2017-08-27 10:58:59 -0700

    Initial Commit

---
+++ TypedTransforms/TypedTransforms.h
@@ -0,0 +1,19 @@
+//
+//  TypedTransforms.h
+//  TypedTransforms
+//
+//  Created by Ray Fix on 8/27/17.
+//  Copyright © 2017 Ray Fix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TypedTransforms.
+FOUNDATION_EXPORT double TypedTransformsVersionNumber;
+
+//! Project version string for TypedTransforms.
+FOUNDATION_EXPORT const unsigned char TypedTransformsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TypedTransforms/PublicHeader.h>
+
+
